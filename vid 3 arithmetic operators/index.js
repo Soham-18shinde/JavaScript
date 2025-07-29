7c8b8f010985a1faeea45c8c2a8c72a190f0c2b8
Soham-18shinde <sohamshinde6254@gmail.com>
2025-07-29 23:42:24 +0530

    Add files via upload

---
+++ vid 3 arithmetic operators/index.js
@@ -0,0 +1,16 @@
+/*let students = 30;
+students **=2
+console.log(students)
+
+
+
+OPERATOR PRECEDENCE
+1. parenthesis()
+2. exponents
+3.multiplication
+4.addition & subtraction
+*/
+
+
+let result= 12 % 5 + 8 /2;
+console.log(result)
