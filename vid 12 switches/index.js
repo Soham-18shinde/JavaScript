f5ffa187f99451b61008d89457cbc9289b2f9c24
Soham-18shinde <sohamshinde6254@gmail.com>
2025-08-19 21:19:50 +0530

    Add files via upload

---
+++ vid 12 switches/index.js
@@ -0,0 +1,15 @@
+let day=9;
+switch(day){
+    case 1:
+    console.log("It is monday")
+    break
+    case 2:
+    console.log("It is Tuesday")
+    break
+    case 3:
+    console.log("It is wednesday")
+    break
+    
+    default:
+        console.log("Think logically dumbass")
+}
