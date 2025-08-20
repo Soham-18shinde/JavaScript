d090d31e7bd24a95fc9a792798d9277b9077c3b0
Soham-18shinde <sohamshinde6254@gmail.com>
2025-08-20 23:28:55 +0530

    Add files via upload

---
+++ vid 13 string methods/index.js
@@ -0,0 +1,16 @@
+
+
+let username=" soham";
+
+//username.charAt(0)  => gives the character present at the mentioned position
+
+//console.log(username.indexOf("o")) => gives the index of the first occurence of the mentioned character
+
+// console.log(username.length)  => gives the length of the mentioned string
+
+// username=username.trim(); => trims the whitesapces of the string
+//console.log(username.toUpperCase())  =>  makes the entire string of uppercase characters
+//console.log(username.toLowerCase())  =>  makes the entire string of lowercase characters
+
+let result= username.startsWith(" ")
+console.log(result)
