bb069a71714b4e4b114e19a57439f44c5e96e574
Soham Shinde <sohamshinde6254@gmail.com>
2025-09-10 23:43:15 +0530

    Add files via upload

---
+++ vid 19 function/index.js
@@ -0,0 +1,33 @@
+
+/*function birthday(username,age)//parametes{
+
+    console.log(" Happy birthday to you")
+    console.log(" Happy birthday to you")
+    console.log(` Happy birthday dear ${username}!!!`)
+    console.log(` Happy birthday ${username} you are ${age} years old!!!`)
+}
+
+birthday("Soham",19)//arguments
+birthday("goku",159)//arguments
+birthday("vk",37)//arguments*/
+
+
+
+
+//function to add 2 numbers 
+
+function add(x,y){
+    
+   return  x+y;
+   
+}
+
+//console.log(add(6,4))
+
+
+function isEven(n){
+
+    return n %2===0? true : false;
+}
+
+console.log(isEven(2))
