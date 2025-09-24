3a3b1011c31736e8a1eb89cd9654b4a53f6cf59a
Soham Shinde <sohamshinde6254@gmail.com>
2025-09-24 23:57:41 +0530

    Add files via upload

---
+++ vid 28 map/index.js
@@ -0,0 +1,28 @@
+
+
+
+/*const nums=[1,2,3,4,5,6]
+
+function square(element){
+
+
+    return Math.pow(element,2)
+
+}
+
+
+const squares=nums.map(square)
+console.log(nums)
+console.log(squares)*/
+
+
+const students=["robert","john","peter","glen"]
+
+
+
+function upperCase(element){
+    return element.toUpperCase();
+}
+
+const studcase= students.map(upperCase)
+console.log(studcase)
