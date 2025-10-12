227bbd58a37fd54bbf3815a2c793f25ebddadf81
Soham Shinde <sohamshinde6254@gmail.com>
2025-10-12 22:29:08 +0530

    Add files via upload

---
+++ vid 36 date objects/index.js
@@ -0,0 +1,12 @@
+/* date objects = objects that conatin vlaues that represent dates and times. these date objects can be changed and formated*/
+
+
+const date= new Date();
+const day= date.getDate()
+const month=date.getMonth()
+const year= date.getFullYear()
+const hour= date.getHours()
+console.log(month)
+console.log(year)
+console.log(day)
+console.log(hour)
