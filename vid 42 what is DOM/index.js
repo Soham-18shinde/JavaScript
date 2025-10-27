7f28238cba4d3b7dbf78a6230e8a1c327895f4f9
Soham Shinde <sohamshinde6254@gmail.com>
2025-10-27 11:46:49 +0530

    Add files via upload

---
+++ vid 42 what is DOM/index.js
@@ -0,0 +1,12 @@
+/*DOM = DOCUMENT OBJECT MODEL 
+        it is a object that represents the page you see in the web browser, and it provides you with an API to interact with it.
+        Web browser constructs the DOM when it loads an HTML Document, and structures all the elements in a tree-like representation.
+        Javascript can access the DOM to Dynamically change the content,structure,and style of a webpage.*/
+        function darkMode(){
+
+        document.body.style.backgroundColor="black"
+        }
+        function lightMode(){
+
+        document.body.style.backgroundColor="white"
+        }
