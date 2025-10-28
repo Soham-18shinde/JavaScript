20461922f491a245a9030eec122931048ff076b7
Soham Shinde <sohamshinde6254@gmail.com>
2025-10-28 20:08:31 +0530

    Add files via upload

---
+++ vid 43 Element Selector/index.js
@@ -0,0 +1,11 @@
+/* element selector = Methods used to target and manipulate HTML elements.They allow you to select one or multiple HTML elements from the DOM
+
+1 document.getElementById() :- ELement or Null
+2 document.getElementByClassName() :- HTML Collection
+3 document.getElementByTagName() :- HTML Collection
+4 document.querySelector() :- ELement or Null
+5 document.querySelectorAll() :- NodeList*/
+
+
+const myHeading = document.getElementById("heading").style.backgroundColor= "cyan";
+myHeading.style.textAlign="center"
