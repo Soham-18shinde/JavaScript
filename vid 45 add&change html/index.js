aa2a81a7eeefc32f95542a2cd8f60cb563d8313d
Soham Shinde <sohamshinde6254@gmail.com>
2025-11-01 23:32:00 +0530

    Add files via upload

---
+++ vid 45 add&change html/index.js
@@ -0,0 +1,6 @@
+const newH1= document.createElement("h1");
+newH1.textContent = " I like Pizza"
+newH1.id="myH1"
+newH1.style.backgroundColor= "cyan"
+newH1.style.color= "gold"
+document.body.prepend(newH1)
